165d2d09e0aa96d4b9aad31d0435af3e7c8dfdd6
Aishwarya011k <aishwaryadshetty019@gmail.com>
2026-04-02 23:24:58 +0530

    Refactor code structure for improved readability and maintainability

--- smart-brain-creation/pages/_document.js
+++ smart-brain-creation/pages/_document.js
@@ -5,6 +5,9 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/footer.png" />
+          <link rel="apple-touch-icon" href="/footer.png" />
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4955812720478714" crossorigin="anonymous"></script>
         </Head>
         <body>
